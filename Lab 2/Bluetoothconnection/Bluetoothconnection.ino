9d6cbba491a86dbfe8aeb1febf37780b1ae5d99a
Fin Jordan 2014 (N0564192) <N0564192@my.ntu.ac.uk>
2018-01-24 13:28:18 +0000

    Wed, Jan 24, 2018  1:28:18 PM

--- Lab 2/Bluetoothconnection/Bluetoothconnection.ino
+++ Lab 2/Bluetoothconnection/Bluetoothconnection.ino
@@ -7,15 +7,20 @@
   Was unsure what you wanted for task 14 of the lab but I understand how to display the message in
   the arduino program and external program like putty.
  */
-int led1 = 11;                          // Use digital pin 11 for the led
+int led2 = 10;
+int led1 = 9;                           // Use digital pin 11 for the led
+int buzzer = 8;
 char inVal;                             // Use this to read a character from the serial port
 
 /****************************************************************************************************************************/
 
 void setup() 
 {
-  pinMode(led1, OUTPUT);                // Initialise digital pin for the LED
-  Serial.begin(115200);                // Set the Baudrate to 115200 on putty as well (note which serial port) 
+  pinMode(led1, OUTPUT);// Initialise digital pin for the LED
+  pinMode(led2, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  Serial.begin(115200);
+  Serial1.begin(115200);// Set the Baudrate to 115200 on putty as well (note which serial port) 
 }
 
 /***************************************************************************************************************************/
@@ -25,21 +30,35 @@ void loop()
   if (Serial.available()>0)            // Will only read the serial when something is entered (have to enter the x twice to stop)
     {
     inVal=Serial.read();               // Reads data from the serial port
-    
-    if(inVal == '1')                    // If you enter 1 the  first LED will turn on then off
+   switch(inVal)
+   { 
+    case 'x':                   // If you enter 1 the  first LED will turn on then off
     {                                   
       digitalWrite(led1, HIGH);         // Turns the LED 1 on
-      Serial.println("You entered 1"); // Prints out this message
+      Serial1.println("You entered x"); // Prints out this message
       delay(2000);                      // Wait for 2 seconds
       digitalWrite(led1, LOW);          // Turns the LED 1 off
+      Serial.println("You entered x, which turned the first LED on for 2 second");
     }
-
-   else                                 // If there is no input from the serial line, turn the LED's off and displays a message
+    break;
   {
+    case 't':
       digitalWrite(led1, LOW);
-      Serial.println("Waiting for command.......");   
+      tone(buzzer, 500, 2000);
+      Serial1.println("Waiting for command......."); 
+      Serial.println("You entered t which makes the buzzer turn on and the second LED flash.");  
+      digitalWrite(led2, LOW);
+      delay(500);
+      digitalWrite(led2, HIGH);
+      delay(500);
+      digitalWrite(led2, LOW);
+      delay(500);
+      digitalWrite(led2, HIGH);
+      delay(500);
+      digitalWrite(led2, LOW);
       delay(2000);
   }
+   }
   }
 }
 
